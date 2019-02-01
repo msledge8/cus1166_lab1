5fef7de34fe8b9e67165de1ad3e1025599e27ec5
msledge <maureen.sledge16@stjohns.edu>
2019-02-01 00:23:29 -0500

    changes

--- playground.py
+++ playground.py
@@ -1,71 +1,78 @@
-print("Practice Basic Program/nHello World") # Display a message
-# Get user input and display a message.
-myname = input("What is your name: ")
-print("Hello %s" % myname)
+print("Practice Basic Program:\nHello World")
+name = input("What is your name? ")
+print("Hello %s" % name)
+print("Practice basic program finished.")
 
+first = 1688
+second = 1.6888888
+third = True
+fourth = None
+print(f"Practice Variables:\nVariable 1: {first}.\nVariable 2: {second}. Its type is {type(second)}")
+print(f"Variable 3: {third}")
+print(f"Variable 4: {fourth}\nPractice variables finished.")
 
-i = 120
-print(f"Variable i has the value {i}")
-f = 1.6180339
-print(f"Variable f has the value {f} and its type is {type(f)}")
-b = True
-print(f"Variable b has the value {b}")
-n = None
-print(f"Variable n has the value of {n}")
-# tuple
-c = (10,20,10)
-print(f" c[0] has the value {c[0]} and is of type: {type(c)}")
-# list
-l = ["Anna", "Tom", "John"]
-l = [10,20,30]
-print(f" l[0] has the value {l[0]} and is of type: {type(l)}")
-# Sets variables.
-s = set()
-s.add(1)
-s.add(4)
-s.add(6)
-print(s)
-# Dictionary
-grades = {"Tom" : "A", "Mark": "B-"}
-grades["Tom"]
-grades["Anna"] = "F"
-# Create an empty dictionary .
-mydictionary = dict()
+tuple1 = (10,20,10)
+print(f"Practice tuple: \ntuple1[0] has the value {tuple1[0]} and is of type: {type(tuple1)}\nPractice tuple finished.")
 
-x=10
-if (x > 0):
-    print("The number x is positive")
-elif (x<0):
-    print("The number x is negative")
+list1 = ["Harry", "Ron", "Hermione"]
+list1 = [1,2,3]
+print(f"Practice List: \nl[0] has the value {list1[0]} and is of type: {type(list1)}\nPractice list finished.")
+
+set1 = set()
+set1.add(1)
+set1.add(4)
+set1.add(6)
+print("Practice set:")
+print(set1)
+print("Practice set finished.")
+
+print("Practice dictionary:")
+grade = dict()
+grade["Hermione"] = "A+"
+grade["Harry"] = "B+"
+grade["Ron"] = "B-"
+print(grade)
+print("Practice dictionary finished.")
+
+print("Practice Conditionals:")
+num = int(input("Enter a number: "))
+if (num > 0):
+    print("The number is positive.")
+elif (num < 0):
+    print("The number is negative.")
 else:
-    print("The number x is zero")
+    print("The number is zero.")
+print("Practice conditionals finished.\nPractice loops:")
 
-for i in range(5):
-    print(i)
-for i_idx, i_value in enumerate(range(2,10)):
-    print(f"{i_idx} - {i_value}" )
-
-def is_even(x):
-    if (x % 2) == 0: return True
-    else: return False
-
-class Book:
-    def __init__(self, title="Software Engineering", isbn=""):
-        self.title = title
-        self.isbn = isbn
-    def printBook(self):
-        print(self.title + ", " + self.isbn)
+for x in range(9):
+    print(x)
+for i, j in enumerate(range(3, 10)):
+    print(f"{i} - {j}")
+print("Practice loops finished.\nPractice functions:")
 
 
-# file: mymodule.helper_utils
-# A function define in the module.
-def square(x):
-    return x*x
-# method to execute file is run from command line.
-def main():
-    pass
-if __name__ == "__main__":
-    main()
+def is_odd(num):
+    if (num % 2) == 0:
+        return False
+    else:
+        return True
 
-from mymodule.helper_utils import square
-print(square(100))
+
+practice = int(input("Enter positive odd number: "))
+print(is_odd(practice))
+print("Practice functions finished.")
+
+
+class Cube:
+    def __init__(self, x):
+        self.x = x
+
+    def print_cube(self):
+        print(f"{self.x} cubed is {self.x * self.x * self.x}.")
+
+
+cubed = Cube(2)
+
+print("Practice class:")
+cubed.print_cube()
+print("Practice class finished.")
