48db1cc0f5f5c78beba4375a7a776f6cae4b535c
msledge <maureen.sledge16@stjohns.edu>
2019-01-31 21:35:30 -0500

    changes

--- mymodules/math_utils.py
+++ mymodules/math_utils.py
@@ -5,4 +5,15 @@ def average_grade(roster):
     sum = 0
     for Student in roster:
         sum += Student.get_grade()
-    print(f"The average is: {sum/len(roster)}.")
+
+    if (sum/len(roster) > 89):
+        letter = "A"
+    elif (sum/len(roster) > 79):
+        letter = "B"
+    elif (sum/len(roster) > 69):
+        letter = "C"
+    elif (sum/len(roster) > 59):
+        letter = "D"
+    else:
+        letter = "F"
+    print(f"The average is: {sum/len(roster)}. The average letter grade was {letter}.")
